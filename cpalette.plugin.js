f4feed9568de30f326ea4e11a06aa57c529067ce
Cain Atkinson <yellowsink@riseup.net>
2022-02-13 20:03:09 +0000

    god that was painful to get workign

--- cpalette.plugin.js
+++ cpalette.plugin.js
@@ -1,6 +1,16 @@
-// A patch to hide the name of cumcord to users of the plugin. May or may not be active.
+/**
+ * @name CommandPalette
+ * @author Yellowsink
+ * @version 1.0.0
+ * @description ye
+ * @source https://github.com/yellowsink/discord-command-palette/blob/src-bd/cpalette.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/yellowsink/discord-command-palette/src-bd/cpalette.plugin.js
+ */
 
-module.exports = () => {
+const PLUGIN_URL = "https://yellowsink.github.io/discord-command-palette/";
+const CC_URL = "https://raw.githubusercontent.com/Cumcord/builds/main/build.js";
+
+const sterilise = () => {
     if (!window.cumcord) {
         console.error("window.cumcord global not found, cannot sterilise.");
         return () => {};
@@ -98,4 +108,46 @@ module.exports = () => {
         unpatchPredicateSections();
         unpatchPluginPage?.();
     };
+};
+
+let unloadCcAfter = true;
+let disablePluginAfter = false;
+let unSterilise;
+
+module.exports = class {
+   async function start() {
+    debugger
+    unloadCcAfter = !window.cumcord;
+
+    if (!window.cumcord) eval(await (await fetch(CC_URL)).text());
+    // wait for cumcord to complete its initialisation
+    // if already loaded, this will do nothing.
+    await cumcord.cum();
+
+    unSterilise = sterilise();
+
+    // load plugins and set state accordingly
+    // unloadCcAfter - if we inject cumcord, remove after
+    // disablePluginAfter - if we enable / install the plugin, disable after in the event we leave cc
+    if (cumcord.plugins.installed.ghost.hasOwnProperty(PLUGIN_URL)) {
+        disablePluginAfter = false;
+
+        if (!cumcord.plugins.loaded.ghost.hasOwnProperty(PLUGIN_URL)) {
+            cumcord.plugins.togglePlugin(PLUGIN_URL);
+            disablePluginAfter = true;
+        }
+    } else {
+        disablePluginAfter = true;
+        await cumcord.plugins.importPlugin(PLUGIN_URL);
+    }
+}
+    stop() {
+    if (unloadCcAfter) {
+        cumcord.uninject();
+    } else if (disablePluginAfter) {
+        cumcord.plugins.togglePlugin(PLUGIN_URL);
+    }
+
+    unSterilise?.();
+}
 };
